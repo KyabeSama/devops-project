f14f704ac99b306e7a7d59a3e88a1cc0f599a3ad
KyabeSama <prosguigui.craft@gmail.com>
2025-01-23 17:14:07 +0100

    Add Ansible playbooks for installing Cert-Manager, NGINX Ingress Controller, and Rancher UI; include roles for managing Helm repositories and Kubernetes namespaces

--- terraform/load-balancer.tf
+++ terraform/load-balancer.tf
@@ -1,32 +1,45 @@
-# resource "aws_iam_role" "lb_controller" {
-#   name = "gros-lb-controller-role"
+resource "aws_lb" "eks" {
+  name               = "eks-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.eks_cluster_sg.id]
+  subnets            = aws_subnet.public.*.id
 
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Effect = "Allow"
-#         Principal = {
-#           Federated = aws_iam_openid_connect_provider.eks.arn
-#         }
-#         Action = "sts:AssumeRoleWithWebIdentity"
-#         Condition = {
-#           StringEquals = {
-#             "${replace(aws_eks_cluster.devops-project.identity[0].oidc[0].issuer, "https://", "")}:sub" = "system:serviceaccount:kube-system:aws-load-balancer-controller"
-#           }
-#         }
-#       }
-#     ]
-#   })
-# }
+  enable_deletion_protection = false
 
-# resource "aws_iam_policy" "lb_controller_policy" {
-#   name        = "gros-lb-controller-policy"
-#   description = "IAM policy for AWS Load Balancer Controller"
-#   policy      = file("load-balancer-policy.json")
-# }
+  tags = {
+    Name = "eks-load-balancer"
+  }
+}
 
-# resource "aws_iam_role_policy_attachment" "lb_controller_attach" {
-#   role       = aws_iam_role.lb_controller.name
-#   policy_arn = aws_iam_policy.lb_controller_policy.arn
-# }
+resource "aws_lb_target_group" "eks" {
+  name     = "eks-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "eks-target-group"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.eks.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.eks.arn
+  }
+}
+
